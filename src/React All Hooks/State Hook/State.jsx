0cadb8882a86276d37fc99df76b5d853125b7d9b
Akshay Neje <akshayneje2823@gmail.com>
2022-05-02 23:33:55 +0530

    All Hooks

--- src/React All Hooks/State Hook/State.jsx
+++ src/React All Hooks/State Hook/State.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 function State() {
     let [count,setCount] = useState(0);
     let incrHandler = () => {
-      setCount(count+1)
+      setCount(count + 1)
     }
   return (
     <div>
