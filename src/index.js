3131a88bb97ed5fbb0dce9e35a9eabba6974da4e
Akshay Neje <akshayneje2823@gmail.com>
2022-08-18 23:08:58 +0530

    asmh

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './custom hooks/App';
+import App from './Project/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
