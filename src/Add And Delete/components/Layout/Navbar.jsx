d8588a6a4f389a906682f28ad99c99c782985517
Akshay Neje <akshayneje2823@gmail.com>
2022-08-20 20:22:44 +0530

    Added Backebd

--- src/Add And Delete/components/Layout/Navbar.jsx
+++ src/Add And Delete/components/Layout/Navbar.jsx
@@ -4,7 +4,7 @@ import { NavLink} from 'react-router-dom'
 export default function Navbar() {
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-success">
                     <a className="navbar-brand" href="/">
                        React User
                     </a>
